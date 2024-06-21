adde111575c24147e8961970e9be9577e1021ffd
Jeyasooriya Saravanan <144498455+Sooriyavela@users.noreply.github.com>
2024-06-21 21:46:00 +0530

    Add files via upload

---
+++ Lab1_5303-3.R
@@ -0,0 +1,89 @@
+# Q1: use sample function to generate a random 20x20 matrix A with integers in [-10, 10]. 
+# note that A corresponds to a system of linear equations
+
+
+
+# Q2: generate a solution vector b with real numbers in [-10, 10]
+# print A and b
+
+
+
+# Q3: print the 2nd row of A
+
+
+
+# Q4: print the 3rd column of A
+
+
+
+# Q5: print the element of A in the 6th row and the 15th column 
+
+
+
+# Q6: combine A and b to one augmented matrix M = [A|b]
+
+
+
+# Q7: Think of a way to double check M is correct. Remember M, A are large size.
+# hint: One possible way: check M by printing out summation of the 1st row of M,A and 1st element of b
+
+
+
+# Q8: solve the equation Ax = b for x
+
+
+
+# Q9: Compute A inverse
+
+
+
+# Q10:compute A inverse multiply b.
+
+
+
+# Q11: check whether A inverse multiply b is the same as the solution of equation Ax = b. Yes or No. 
+
+
+
+# Q12: Compute the reduced echelon form of M
+
+
+
+# Q13: genterate another vector b1 with real numbers in [-10, 10]
+
+
+
+# Q14: use random package to to generate a random 20x20 matrix A with integers in [11, 20]
+
+
+
+# Q15: compute A+B and think of a method to check the correctness
+
+
+
+
+# Q16: compute A*B and think of a method to check the correctness
+
+
+
+# Q17: compute A transpose and think of a method to check the correctness
+
+
+
+# Q18: verify transpose of A + B equal to transpose A + transpose B 
+
+
+
+# Q19: verify transpose of A multiply B equal to transpose B multiply transpose A 
+
+
+
+
+# Q20: create a 20x20, I20, identity matrix
+
+
+
+# Q21: create matrix M2 = [A|I20]
+# compute the reduced echelon form of M2
+# obtain inverse of A from reduced echelon form of M2
+
